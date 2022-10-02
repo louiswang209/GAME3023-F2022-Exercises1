48eab980cf21ad7eeddb6549db0052d54ba6965e
Zijie Wang <ludovicus1057860553@gmail.com>
2022-10-02 16:37:18 -0400

    Added code for the player to use the portal to go to another scene

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject playerPrefeb;
+
+    //private static Traveler player = null;
+    public static Traveler Player
+    {
+        get;
+        private set;
+        //get { return player; }
+        //private set { }
+    }
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Player == null)
+        {
+            GameObject newPlayer = Instantiate(playerPrefeb, transform.position, Quaternion.identity);
+            Player = newPlayer.GetComponent<Traveler>();
+        }
+    }
+
+    
+}
--- Assets/Traveler.cs
+++ Assets/Traveler.cs
@@ -7,7 +7,7 @@ public class Traveler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
